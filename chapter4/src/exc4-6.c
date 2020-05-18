c0122c240517f76d0e59d7012c4d92ccc01a179d
aviouslyak <avichailevenson@gmail.com>
2020-05-18 14:04:55 -0800

    finished exc4-6

--- chapter4/src/exc4-6.c
+++ chapter4/src/exc4-6.c
@@ -1,85 +1,115 @@
-/* Exercise 4-6. Add comands for handling variables. (It's easy to provide twenty-six variable single-letter names.)
- * Add a variable for the most recently printed value.
- */
+/* Exercise 4-5. Add access to library functions like sin, exp, and pow. See <math.h> in Appendix B, section 4. */
 #include <ctype.h>
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#define MAXOP   100
-#define NUMBER  '0'
-#define MAXVAL  100
-#define BUFSIZE 100
-#define SIN     '!'
-#define COS     '"'
-#define TAN     '#'
-#define POW     '^'
-#define EXP     '$'
-#define JUNK    '|'
-#define VAR     '?'
-#define UNSET   '@'
+
+#define MAXOP       100
+#define NUMBER      '0'
+#define MAXVAL      100
+#define BUFSIZE     100
+#define SIN         '!'
+#define COS         '"'
+#define TAN         '#'
+#define POW         '^'
+#define EXP         '$'
+#define JUNK        '|'
+#define UNDEFINED   '('
+#define NOTVALID    ')'
+#define LASTUSEDVAR '@'
 
 int getop(char[]);
 void push(double);
 double pop(void);
 double peek(void);
+double last_used = 0;
 
 int main(int argc, char* argv[]) {
     int type;
     double op2;
     char s[MAXOP];
+    double val;
+
     while ((type = getop(s)) != EOF) {
         switch (type) {
         case NUMBER:
-            push(atof(s));
+            val = atof(s);
+            last_used = val;
+            push(val);
             break;
         case '+':
-            push(pop() + pop());
+            val = pop() + pop();
+            last_used = val;
+            push(val);
             break;
         case '*':
-            push(pop() * pop());
+            val = pop() * pop();
+            last_used = val;
+            push(val);
             break;
         case '-':
             op2 = pop();
-            push(pop() - op2);
+            val = pop() - op2;
+            last_used = val;
+            push(val);
             break;
         case '/':
             op2 = pop();
             if (op2 != 0.0) {
-                push(pop() / op2);
+                val = pop() / op2;
+                last_used = val;
+                push(val);
             }
             break;
         case '%':
             op2 = pop();
-            push((int)pop() % (int)op2);
+            val = (int)pop() % (int)op2;
+            push(val);
             break;
+
         case POW:
             op2 = pop();
             double val = pow(pop(), op2);
+            last_used = val;
             push(val);
             break;
+
         case TAN:
-            push(tan(pop()));
-            break;
-        case EXP:
-            push(exp(pop()));
-            break;
-        case COS:
-            push(cos(pop()));
-            break;
-        case SIN:
-            push(sin(pop()));
+            val = (tan(pop()));
+            last_used = val;
+            push(val);
             break;
 
-        case UNSET:
-            printf("variable not assigned\n");
+        case EXP:
+            val = (exp(pop()));
+            last_used = val;
+            push(val);
             break;
+
+        case COS:
+            val = (cos(pop()));
+            last_used = val;
+            push(val);
+            break;
+
+        case SIN:
+            val = (sin(pop()));
+            last_used = val;
+            push(val);
+            break;
+
+        case NOTVALID:
+            printf("error: Unvalid assignment\n");
+            break;
+        case UNDEFINED:
+            printf("error: Undefined");
         case '\n':
             printf("\t%.8g\n", pop());
             break;
+
         default:
             printf("error: Unknown command\n");
-            break;
         }
     }
     return 0;
@@ -121,7 +151,7 @@ int getch(void);
 void ungetch(int);
 
 int vars[26];
-int set[26] = {0};
+int filled[26] = {0};
 
 /* ftoc: translates function name into one letter char */
 char ftoc(char s[]) {
@@ -139,60 +169,74 @@ char ftoc(char s[]) {
         return JUNK;
     }
 }
+
 /* getop: get next operator or numeric operand */
 int getop(char s[]) {
-    int i, c, var;
+    int i, c, validNum, varName;
 
     while ((s[0] = c = getch()) == ' ' || c == '\t') {
         ;
     }
 
     s[1] = '\0';
-    if (!isdigit(c) && c != '.' && c != '-' && !isalpha(c)) {
-        return c; // not a number } i = 0;
+    if (c != LASTUSEDVAR && !isdigit(c) && c != '.' && c != '-' && !isalpha(c)) {
+        return c; // not a number
     }
 
     i = 0;
 
     if (isalpha(c)) {
-        c = tolower(c);
+        s[0] = tolower(c); // ignore case, rewrite in lower case
         while (isalpha(s[++i] = c = tolower(getch()))) {
             ;
         }
+
         s[i] = '\0';
 
-        if (strlen(s) == 1) { /* Is it a variable */
+        if (strlen(s) == 1) { // assignment or recall
 
-            if (c == '=') {
-                i = 0;
+            varName = s[0];
+
+            if (c == '=') { // assignment
+                // get value
+                validNum = 0;
                 c = getch();
-                printf("c = %c\n", c);
-                var = s[0];
+                i = 0;
+
+                if (c == '-') {
+                    c = getch();
+                    if (!isdigit(c) && c != '.') {
+                        ungetch(c);
+                        return '-';
+                    }
+                    s[0] = c;
+                    ungetch(c);
+                }
 
                 if (isdigit(c)) {
-                    s[i] = c;
+                    s[0] = c;
+                    validNum = 1;
                     while (isdigit(s[++i] = c = getch())) {
                         ;
                     }
                 }
 
                 if (c == '.') {
-                    s[i] = c;
+                    s[0] = c;
+                    validNum = 1;
                     while (isdigit(s[++i] = c = getch())) {
                         ;
                     }
                 }
-
                 s[i] = '\0';
-                
 
-//                if (!isdigit(s[0]) || s[0] != '.') {
- //                   return c;
- //               }
+                if (!validNum) {
+                    ungetch(c);
+                    return NOTVALID;
+                }
 
-                printf("s = %s\n", s); 
-                set[var - 'a'] = 1;
-                vars[var - 'a'] = atof(s);
+                vars[varName - 'a'] = atof(s);
+                filled[varName - 'a'] = 1;
 
                 if (c != EOF) {
                     ungetch(c);
@@ -201,22 +245,17 @@ int getop(char s[]) {
                 return NUMBER;
             }
 
-            if (set[s[0] - 'a'] != 0) {
-                sprintf(s, "%f", (double)vars[s[0] - 'a']);
-
+            else {
                 if (c != EOF) {
                     ungetch(c);
                 }
-
-                return NUMBER;
+                if (varName >= 'a' && varName <= 'z' && filled[varName - 'a'] == 1) {
+                    sprintf(s, "%g", (double)vars[varName - 'a']);
+                    return NUMBER;
+                } else {
+                    return UNDEFINED;
+                }
             }
-
-            if (c != EOF) {
-
-                ungetch(c);
-            }
-
-            return UNSET;
         }
 
         if (c != EOF) {
@@ -226,7 +265,12 @@ int getop(char s[]) {
         return ftoc(s);
     }
 
-    if (c == '-') {
+    if (c == LASTUSEDVAR) {
+        sprintf(s, "%g", (double)last_used);
+        return NUMBER;
+    }
+
+    if (c == '-') { // is c negative or a subtraction sign
         c = getch();
         if (!isdigit(c) && c != '.') {
             ungetch(c);
@@ -250,7 +294,6 @@ int getop(char s[]) {
     if (c != EOF) {
         ungetch(c);
     }
-
     return NUMBER;
 }
 
