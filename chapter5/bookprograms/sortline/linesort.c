8e310a2915c5a98f460f07459a321b6e31df4bac
aviouslyak <avichailevenson@gmail.com>
2020-05-30 22:50:19 -0800

    Fixed comments in linesort

--- chapter5/bookprograms/sortline/linesort.c
+++ chapter5/bookprograms/sortline/linesort.c
@@ -5,19 +5,18 @@
 #define MAXLEN    1000  /* max length of any input line */
 #define ALLOCSIZE 10000 /* size of available space */
 
-char* lineptr[MAXLINES]; /* pointers to text lines */
+char *lineptr[MAXLINES]; /* pointers to text lines */
 
-int readlines(char* lineptr[], int nlines);
-void writelines(char* lineptr[], int nlines);
+int readlines(char *lineptr[], int nlines);
+void writelines(char *lineptr[], int nlines);
 
-void qsort(char* lineptr[], int left, int right);
+void qsort(char *lineptr[], int left, int right);
 
 /* sort input lines */
 int main() {
     int nlines;
     if ((nlines = readlines(lineptr, MAXLINES)) >= 0) {
         qsort(lineptr, 0, nlines - 1);
-        printf("here\n");
         writelines(lineptr, nlines);
         return 0;
     } else {
@@ -26,11 +25,11 @@ int main() {
     }
 }
 
-int getLine(char*, int);
-char* alloc(int);
+int getLine(char *, int);
+char *alloc(int);
 
 /* readlines: read input lines */
-int readlines(char* lineptr[], int maxlines) {
+int readlines(char *lineptr[], int maxlines) {
     int len, nlines;
     char *p, line[MAXLEN];
 
@@ -39,7 +38,7 @@ int readlines(char* lineptr[], int maxlines) {
         if (nlines >= maxlines || (p = alloc(len)) == NULL) {
             return -1;
         } else {
-            // line[len - 1] = '\0';
+            line[len - 1] = '\0';
             strcpy(p, line);
             lineptr[nlines++] = p;
         }
@@ -48,7 +47,7 @@ int readlines(char* lineptr[], int maxlines) {
 }
 
 /* writelines: write output lines */
-void writelines(char* lineptr[], int nlines) {
+void writelines(char *lineptr[], int nlines) {
     int i;
 
     for (i = 0; i < nlines; i++) {
@@ -61,18 +60,18 @@ int getLine(char s[], int lim) {
     int c, i;
 
     for (i = 0; i < lim - 1 && (c = getchar()) != EOF && c != '\n'; ++i) s[i] = c;
-    /*  if (c == '\n') {
-            s[i] = c;
-            ++i;
-        }*/
+    if (c == '\n') {
+        s[i] = c;
+        ++i;
+    }
     s[i] = '\0';
     return i;
 }
 
 /* qsort: sort v[left]...v[right] into increasing order */
-void qsort(char* v[], int left, int right) {
+void qsort(char *v[], int left, int right) {
     int i, last;
-    void swap(char* v[], int i, int j);
+    void swap(char *v[], int i, int j);
 
     if (left >= right) return;
     swap(v, left, (left + right) / 2);
@@ -85,17 +84,17 @@ void qsort(char* v[], int left, int right) {
 }
 
 /* swap: interchange v[i] and v[j] */
-void swap(char* v[], int i, int j) {
-    char* temp;
+void swap(char *v[], int i, int j) {
+    char *temp;
     temp = v[i];
     v[i] = v[j];
     v[j] = temp;
 }
 
 static char allocbuf[ALLOCSIZE]; /* storage for alloc */
-static char* allocp = allocbuf;  /* next free position */
+static char *allocp = allocbuf;  /* next free position */
 
-char* alloc(int n) {                          /* returns pointer to n characters */
+char *alloc(int n) {                          /* returns pointer to n characters */
     if (allocbuf + ALLOCSIZE - allocp >= n) { /* it fits */
         allocp += n;
         return allocp - n; /* old p */
